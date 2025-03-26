07f239cf9b7437d88aed3390c1b70315c99be750
mjedrzejowski2 <mjedrzejowski00@gmail.com>
2025-03-26 17:42:17 +0100

    Feature: Post request - add product

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,1 +1,22 @@
 import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true // give createdAt and updatedAt
+})
+
+const Product = mongoose.model('Product', productSchema) // Product -->products mongose will take this and make it lower case and make it plural as well
+
+export default Product
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js'
 
 dotenv.config();
 
 const app = express()
 const PORT = 8383
 
-app.get("/products", (req,res) => {
+app.use(express.json()) // TO MAKE SURE THAT POST WITH JSON WORKS, allow us to accept data in the req.body --> middleware
+
+app.get("/api/products", (req,res) => {
     res.send("Server is ready")
+})
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body; // 
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success:false, message: "Please provide all fields"})
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+        res.status(201).json({ success:true, data: newProduct})
+    } catch (error) {
+        console.log("Error in create product", error.message)
+        res.status(500).json({ success: false, message: "Server error"})
+    }
+    
 })
 
 console.log(process.env.MONGO_URI)
